e6c407415704db2be7c45fda595b51c7f08108c6
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-13 07:19:24 +0530

    rendering an error message

---
+++ index.js
@@ -0,0 +1,9 @@
+let errorParagraph = document.getElementById("error")
+console.log(consoleParagraph)
+
+function purchase()
+{
+     console.log("button clicked")
+     errorParagraph.textContent = "Something went wrong, please try again"
+}
+
