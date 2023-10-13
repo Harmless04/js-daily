0b38d9d15e7c80af469a3bf0a145fe95dccd5c73
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-13 16:32:38 +0530

    basic if else file

---
+++ ifelseif.js
@@ -0,0 +1,12 @@
+let age=29
+if (age===24)
+{
+  console.log("happy 24th birthday")
+}
+else if(age>24){
+  console.log("not your 24th birthday , you are old")
+}
+else if (age<24)
+{
+    console.log("you are still younger than 24")
+}
