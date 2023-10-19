45c0ab3bee898b87458bc0de9f4c8651a5c5f7a3
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-19 08:30:54 +0530

    fun challenge with basic push pop function

---
+++ arraypushpop.js
@@ -0,0 +1,17 @@
+let cards = [3,4]
+cards.push(9)
+console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages=["hey how's it going",
+               "i'm great thank",
+               "all good been working on my portfolio lately"
+               ]
+
+    let newMessage = "wow that's nice"
+    messages.push(newMessage)
+    console.log(messages)
+
+    messages.pop()
+    console.log(messages)
