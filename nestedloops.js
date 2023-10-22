539c61c318ca04480ca5df3aa5fcf905eb27c2fb
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-22 09:12:46 +0530

    basic nested loop problem with boolean

---
+++ nestedloops.js
@@ -0,0 +1,12 @@
+let hasCompleted = true
+let givesCertificate = true
+if(hasCompleted===true){
+  if(givesCertificate===true){
+    generateCertificate()
+  }
+}
+
+
+function generateCertificate (){
+  console.log("generating certificate....")
+}
