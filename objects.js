3f49185f8fa3d9f3fb164a73e5e7fb46a19c0536
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-22 13:55:48 +0530

    objects and syntax
    objects airbnb castle listing and console logging them out

---
+++ objects.js
@@ -0,0 +1,10 @@
+let airbnbCastle = 
+{
+    owner:"harmless04",
+    place:"wellington island",
+    isPaid:true,
+    rent: "1500$ per hour",
+    hoursBooked: 24,
+    
+}
+console.log(airbnbCastle.owner)
