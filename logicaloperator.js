37939cfe8c2a3c40122241ce7abbef6878a636f1
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-22 09:29:56 +0530

    simple problem
    uses logical operator &&

---
+++ logicaloperator.js
@@ -0,0 +1,12 @@
+let hasSolved=false
+let hasHints=false
+
+if(hasSolved===false && hasHints===false)
+{
+  showSolution()
+}
+
+
+function showSolution (){
+  console.log("showing solution....")
+}
