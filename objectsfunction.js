6c6e913d17543c44fd643e049b78a7a203de6615
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-25 19:46:13 +0530

    objects and function trial

---
+++ objectsfunction.js
@@ -0,0 +1,13 @@
+let person = 
+{
+  name:"Harmy",
+  age:19,
+  country : "india"
+}
+function logData()
+{
+  console.log(person.name+" is "+person.age+" and living  in "+person.country)  
+
+}
+
+logData()
